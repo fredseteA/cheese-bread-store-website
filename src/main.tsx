beb19aa303210c76f05f07ae96139d15776d589b
unknown <fredericoluna93@gmail.com>
2026-03-16 10:15:19 -0300

    fix: add missing index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "@/app/App"
-import "./index.css";
+import App from "@/app/App";
+import "@/layouts/index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
